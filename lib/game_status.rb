bca67507b791e73d4bb819c5fd1fbcbab2e8c197
Chris Nunez <cenunez47@gmail.com>
2017-09-17 01:47:47 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,8 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.detect do |x|
   board[x[0]] == board[x[1]] &&
-  board[x[1]] ==  board[x[2]]
+  board[x[1]] ==  board[x[2]] &&
+  position_taken?(board, x[0])
   end
 end
 
