26cc5e900c1e30c8b0e5a481d5456eefd1c8d86f
Chris Nunez <cenunez47@gmail.com>
2017-09-17 00:37:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,14 +6,14 @@ end
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
-  [0, 1, 2],  # 0 Top row
-  [3, 4, 5],  # 1 Middle row
-  [6, 7, 8],  # 2 Bottom row
-  [0, 3, 6],  # 3 Left row
-  [1, 4, 7],  # 4 Middle[Vertical] row
-  [2, 5, 8],  # 5 Right row
-  [0, 4, 8],  # 6 First Diagnol
-  [6, 4, 2]   # 7 Second Diagnol
+  [0, 1, 2],  # 0
+  [3, 4, 5],  # 1
+  [6, 7, 8],  # 2
+  [0, 3, 6],  # 3
+  [1, 4, 7],  # 4
+  [2, 5, 8],  # 5
+  [0, 4, 8],  # 6
+  [6, 4, 2]   # 7
 ]
 
 def won?(board)
@@ -21,7 +21,7 @@ def won?(board)
     board[combi[0]] == board[combi[1]] &&
     board[combi[1]] ==  board[combi[2]] &&
     position_taken?(board, combi[0])
-  end #do
+  end
 end
 
 def full?(board)
